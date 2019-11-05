cf3f2dd78425601cbe43eaceaa5ed2c4ff103b26
Gherzan Catalina-Sorina <cgherzan@frigoglass.com>
2019-11-05 07:09:31 +0200

    more style

--- ex2shop/src/components/Basket.js
+++ ex2shop/src/components/Basket.js
@@ -1,7 +1,7 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
-import BuyItem from "./BuyItem";
+import BuyItem from './BuyItem';
 
 // const ImgBar = styled.img`
 //   width: 90vw;
@@ -10,77 +10,77 @@ import BuyItem from "./BuyItem";
 // `;
 
 const TrHead = styled.tr`
-  width: 90vw;
-  background-color: gray;
-  border-radius: 1rem;
-  font: 1.5rem Cursive;
-  padding: 1rem;
-  display: flex;
-  align-items: flex-end;
+	width: 90vw;
+	background-color: gray;
+	border-radius: 1rem;
+	font: 1.5rem Cursive;
+	padding: 1rem;
+	display: flex;
+	justify-content: space-around;
 
-  th {
-    color: white;
-    padding: 0.5rem;
-    background-color: transparent;
-  }
+	th {
+		color: white;
+		padding: 0.5rem;
+		background-color: transparent;
+	}
 `;
 
 const TrBottom = styled.tr`
-  width: 90vw;
-  background-color: #4b7891;
-  border-radius: 1rem;
-  font: 1.5rem Cursive;
-  padding: 1rem;
-  color: white;
-  text-align: right;
+	width: 90vw;
+	background-color: #4b7891;
+	border-radius: 1rem;
+	font: 1.5rem Cursive;
+	padding: 1rem;
+	color: white;
+	text-align: right;
 `;
 
 const Table = styled.table`
-  margin: auto;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 98vw;
-  border-radius: 1rem;
+	margin: auto;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	width: 98vw;
+	border-radius: 1rem;
 `;
 
 const Div = styled.div`
-  height: 15vh;
-  width: 98vw;
-  padding: 1.5rem;
-  overflow: auto;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+	height: 14vh;
+	width: 98vw;
+	padding: 1.5rem;
+	overflow: auto;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 `;
 
-const Basket = props => {
-  return (
-    props.basket && (
-      <React.Fragment>
-        <Table>
-          <TrHead>
-            <th>Product</th>
-            <th>Price</th>
-            <th>Quantity</th>
-            <th>Total price</th>
-          </TrHead>
-          <Div>
-            {props.basket.map(item => (
-              <BuyItem
-                key={item.id}
-                item={item}
-                addItem={() => props.addItem(item)}
-                removeItem={() => props.removeItem(item.id)}
-              />
-            ))}
-          </Div>
-          <TrBottom>{props.price}</TrBottom>
-        </Table>
-      </React.Fragment>
-    )
-  );
+const Basket = (props) => {
+	return (
+		props.basket && (
+			<React.Fragment>
+				<Table>
+					<TrHead>
+						<th>Product</th>
+						<th>Price</th>
+						<th>Quantity</th>
+						<th>Total price</th>
+					</TrHead>
+					<Div>
+						{props.basket.map((item) => (
+							<BuyItem
+								key={item.id}
+								item={item}
+								addItem={() => props.addItem(item)}
+								removeItem={() => props.removeItem(item.id)}
+							/>
+						))}
+					</Div>
+					<TrBottom>{props.price}</TrBottom>
+				</Table>
+			</React.Fragment>
+		)
+	);
 };
 
 export default Basket;
--- ex2shop/src/components/BuyItem.js
+++ ex2shop/src/components/BuyItem.js
@@ -1,36 +1,49 @@
-import React from "react";
-import styled from "styled-components";
-import BackgroundAddImage from "../assets/button_add.png";
-import BackgroundRemoveImage from "../assets/button_remove.png";
+import React from 'react';
+import styled from 'styled-components';
+import BackgroundAddImage from '../assets/button_add.png';
+import BackgroundRemoveImage from '../assets/button_remove.png';
 
 const ButonAdd = styled.button`
-  background-image: url(${BackgroundAddImage});
+	background-image: url(${BackgroundAddImage});
+	border: none;
+	background-color: transparent;
+	margin: 0.2rem;
 `;
 const ButonRemove = styled.button`
-  background-image: url(${BackgroundRemoveImage});
+	background-image: url(${BackgroundRemoveImage});
+	border: none;
+	background-color: transparent;
+	margin: 0.2rem;
+`;
+const Tr = styled.tr`
+	width: 90vw;
+	height: 2rem;
+	background-color: #88a792;
+	border-radius: 1rem;
+	font: 1.5rem Cursive;
+	padding: 0.2rem;
+	margin: 0.4rem;
+	display: flex;
+	justify-content: space-around;
 `;
 
-const BuyItem = props => {
-  return (
-    <tr>
-      <td>
-        <img alt="img" src={props.item.imageUrl} onClick={props.addItem} />
-      </td>
-      <td>{props.item.name}</td>
-      <td>{props.item.price}</td>
-      <td>{props.item.qty}</td>
-      <td>
-        <ButonAdd onClick={props.addItem} className="add">
-          Add
-        </ButonAdd>
-      </td>
-      <td>
-        <ButonRemove onClick={props.removeItem} className="remove">
-          Remove
-        </ButonRemove>
-      </td>
-    </tr>
-  );
+const BuyItem = (props) => {
+	return (
+		<Tr>
+			<td>
+				<img alt="img" src={props.item.imageUrl} onClick={props.addItem} />
+			</td>
+			<td>{props.item.name}</td>
+			<td>{props.item.price}</td>
+			<td>{props.item.qty}</td>
+			<ButonAdd onClick={props.addItem} className="add">
+				Add
+			</ButonAdd>
+			<ButonRemove onClick={props.removeItem} className="remove">
+				Remove
+			</ButonRemove>
+		</Tr>
+	);
 };
 
 export default BuyItem;
