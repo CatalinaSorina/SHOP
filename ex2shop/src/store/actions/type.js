06a4c26da3234319e7c63760b6455a783d8a4b8f
Gherzan Catalina-Sorina <cgherzan@frigoglass.com>
2019-11-05 08:36:15 +0200

    Updates

--- ex2shop/src/store/actions/type.js
+++
@@ -1,5 +0,0 @@
-export const ADD_TO_BASKET = 'ADD_TO_BASKET';
-
-export const ADD_QTY_TO_ITEM = 'ADD_QTY_TO_ITEM';
-
-export const REMOVE_ITEM = 'REMOVE_ITEM';
---
+++ precache-manifest.28260f8a101f6029fa00ac56f909e7d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "386204249e95350bcfcd9a838ee7e8f6",
+    "url": "/SHOP/index.html"
+  },
+  {
+    "revision": "4f1ee8747cbc0d29da02",
+    "url": "/SHOP/static/css/main.a687ca69.chunk.css"
+  },
+  {
+    "revision": "478fc8f0b134860f1bd0",
+    "url": "/SHOP/static/js/2.edac3255.chunk.js"
+  },
+  {
+    "revision": "4f1ee8747cbc0d29da02",
+    "url": "/SHOP/static/js/main.e093b1f6.chunk.js"
+  },
+  {
+    "revision": "4b8af2f6ca43add9abca",
+    "url": "/SHOP/static/js/runtime-main.9a63e2b5.js"
+  }
+]);
